1058455565a563d0c65e596c57e0398c0a06d663
crane <crane.resigned@qq.com>
2023-09-01 14:55:22 +0800

    v6.0.2，模式显示，生成密码

--- src/main/java/crane/model/service/SecurityService.java
+++ src/main/java/crane/model/service/SecurityService.java
@@ -194,6 +194,7 @@ public final class SecurityService {
      * @Date 2023-05-24 16:52:33
      */
     public static String generateRandomStrongPassword() {
+
 //        return new SnowflakeGenerator().next().toString();
         /**
          * 安全服务内部类
